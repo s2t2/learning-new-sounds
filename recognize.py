2996648c7a4402fc4d7ac23d176402a4fa9d2c6c
MJ Rossetti <s2t2@users.noreply.github.com>
2019-01-19 18:02:05 -0500

    Use hosted audio

--- recognize.py
+++ recognize.py
@@ -3,40 +3,39 @@
 from dotenv import load_dotenv
 import io
 import os
-import pdb
+import pdb  # pdb.set_trace()
 from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
 
 load_dotenv() # recognize GOOGLE_APPLICATION_CREDENTIALS from .env file
 
-#pdb.set_trace()
-
 CREDENTIALS_FILEPATH = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 
-print("---------------------------")
-print(f"CREDENTIALS FILE: {CREDENTIALS_FILEPATH}")
-print("---------------------------")
+print(f"\nCREDENTIALS FILE: {CREDENTIALS_FILEPATH}\n")
 
 client = speech.SpeechClient()
 
-config = types.RecognitionConfig(
-    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=16000,
-    language_code="en-US"
-)
+config = {
+    "encoding": "FLAC",
+    "sample_rate_hertz": 16000,
+    "language_code": "en-US",
+    "enable_word_time_offsets": False
+}
 
-file_name = os.path.join(os.path.dirname(__file__), "resources", "audio.raw")
-
-with io.open(file_name, 'rb') as audio_file:
-    content = audio_file.read()
-    audio = types.RecognitionAudio(content=content)
+audio = {
+    "uri": "gs://cloud-samples-tests/speech/brooklyn.flac"
+}
 
 response = client.recognize(config, audio)
 
-print("---------------------------")
-print(f"RESPONSE (#{type(response)}):")
-print("---------------------------")
+print(f"\nRESPONSE: {type(response)})\n") #> <class 'google.cloud.speech_v1.types.RecognizeResponse'>)
+print(f"\nRESULTS: {type(response.results)})\n") #> <class 'google.protobuf.pyext._message.RepeatedCompositeContainer'>)
 
 for result in response.results:
-    print( f"Transcript: #{result.alternatives[0].transcript}")
+    print(f"\nRESULT: {type(result)})\n") #> <class 'google.cloud.speech_v1.types.SpeechRecognitionResult'>)
+    print(f"\nALTERNATIVES: {type(result.alternatives)})\n") #> <class 'google.protobuf.pyext._message.RepeatedCompositeContainer'>)
+
+    for a in result.alternatives:
+        print(a)
+        #print(f"\n  TRANSCRIPT: '{a.transcript.title()}' | CONFIDENCE: {a.confidence}\n") #> TRANSCRIPT: 'How Old Is The Brooklyn Bridge' | CONFIDENCE: 0.9833518266677856
