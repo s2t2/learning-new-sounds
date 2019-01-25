f7ecd3cecfe05cf0ed20e4173a3c49d1cd0d299e
MJ Rossetti <s2t2@users.noreply.github.com>
2019-01-25 14:38:43 -0500

    Differentiate between spanish sounds

--- scripts/recognize_local.py
+++ scripts/recognize_local.py
@@ -9,6 +9,8 @@ import pdb # pdb.set_trace()
 import random
 import speech_recognition as sr
 
+#> see: http://www.lingoes.net/en/translator/langcode.htm
+LANG = os.environ.get("LANG", "en-US")
 
 AUDIO_FILENAME = os.environ.get("AUDIO_FILENAME", "brooklyn.flac") # random.shuffle(["brooklyn.flac", "french.aiff" "chinese.flac"])
 AUDIO_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "sounds", AUDIO_FILENAME)
@@ -24,7 +26,7 @@ try:
     #transcript = client.recognize_google(audio) #> 'how old is the Brooklyn Bridge'
     #print("TRANSCRIPT: " + transcript)
 
-    response = client.recognize_google(audio, show_all=True) #> {'alternative': [{'transcript': 'how old is the Brooklyn Bridge', 'confidence': 0.987629}], 'final': True}
+    response = client.recognize_google(audio, language=LANG, show_all=True) #> {'alternative': [{'transcript': 'how old is the Brooklyn Bridge', 'confidence': 0.987629}], 'final': True}
     alternative = response["alternative"][0]
     print("TRANSCRIPT: " + alternative["transcript"])
     print("CONFIDENCE: " + str(alternative["confidence"]))
