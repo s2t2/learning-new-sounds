2079dc24d64ce76cf7262d26d31240c08ce37f14
hiepnguyen034 <hiepnguyen034@gmail.com>
2019-01-25 14:29:14 -0500

    combining files

---
+++ scripts/record_and_save.py
@@ -0,0 +1,46 @@
+import os
+import speech_recognition as sr
+import pdb # pdb.set_trace()
+import random
+
+
+def main():
+	client = sr.Recognizer() # are there config options here?
+
+	with sr.Microphone() as mic:
+	    print("Say something!")
+	    audio = client.listen(mic) # are there config options here?
+
+	AUDIO_FILE = os.path.join(os.path.dirname(__file__), "..", "sounds", "microphone-results.flac") # path.join(SOUNDS_DIR, random.shuffle(["brooklyn.flac", "french.aiff" "chinese.flac"]))
+
+	with open(AUDIO_FILE, "wb") as f:
+	    f.write(audio.get_flac_data())
+
+
+	AUDIO_FILENAME = os.environ.get("AUDIO_FILENAME", "microphone-results.flac") # random.shuffle(["brooklyn.flac", "french.aiff" "chinese.flac"])
+	AUDIO_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "sounds", AUDIO_FILENAME)
+
+	#pdb.set_trace()
+
+	client = sr.Recognizer()
+
+	with sr.AudioFile(AUDIO_FILEPATH) as source:
+	    audio = client.record(source)
+
+	try:
+	    #transcript = client.recognize_google(audio) #> 'how old is the Brooklyn Bridge'
+	    #print("TRANSCRIPT: " + transcript)
+
+	    response = client.recognize_google(audio, show_all=True) #> {'alternative': [{'transcript': 'how old is the Brooklyn Bridge', 'confidence': 0.987629}], 'final': True}
+	    alternative = response["alternative"][0]
+	    print("TRANSCRIPT: " + alternative["transcript"])
+	    print("CONFIDENCE: " + str(alternative["confidence"]))
+
+	except Exception as e:
+	    print("TRANSCRIPT ERROR:", e)
+
+
+if __name__ == '__main__':
+	main()
+
+
