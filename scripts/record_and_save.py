330544cb558b89a845266abf6359e445416deb4b
MJ Rossetti <s2t2@users.noreply.github.com>
2019-01-27 17:56:13 -0500

    Simplify, avoid reading audio more than once

--- scripts/record_and_save.py
+++ scripts/record_and_save.py
@@ -3,29 +3,36 @@ import speech_recognition as sr
 import pdb # pdb.set_trace()
 import random
 
+AUDIO_FILENAME = os.environ.get("AUDIO_FILENAME", "microphone-results.flac") # random.shuffle(["brooklyn.flac", "french.aiff" "chinese.flac"])
+AUDIO_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "sounds", AUDIO_FILENAME)
 
 def main():
+
 	client = sr.Recognizer() # are there config options here?
+
+	#
+	# READ AUDIO FROM MICROPHONE
+	#
 
 	with sr.Microphone() as mic:
 	    print("Say something!")
 	    audio = client.listen(mic) # are there config options here?
 
-	AUDIO_FILE = os.path.join(os.path.dirname(__file__), "..", "sounds", "microphone-results.flac") # path.join(SOUNDS_DIR, random.shuffle(["brooklyn.flac", "french.aiff" "chinese.flac"]))
+	#
+	# WRITE AUDIO TO FILE
+	#
 
-	with open(AUDIO_FILE, "wb") as f:
+	with open(AUDIO_FILEPATH, "wb") as f:
 	    f.write(audio.get_flac_data())
 
-
-	AUDIO_FILENAME = os.environ.get("AUDIO_FILENAME", "microphone-results.flac") # random.shuffle(["brooklyn.flac", "french.aiff" "chinese.flac"])
-	AUDIO_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "sounds", AUDIO_FILENAME)
-
-	#pdb.set_trace()
+	#
+	# RECOGNIZE WHAT WAS SAID
+	#
 
 	client = sr.Recognizer()
 
-	with sr.AudioFile(AUDIO_FILEPATH) as source:
-	    audio = client.record(source)
+	# with sr.AudioFile(AUDIO_FILEPATH) as source:
+	#     audio = client.record(source)
 
 	try:
 	    #transcript = client.recognize_google(audio) #> 'how old is the Brooklyn Bridge'
@@ -39,8 +46,5 @@ def main():
 	except Exception as e:
 	    print("TRANSCRIPT ERROR:", e)
 
-
 if __name__ == '__main__':
 	main()
-
-
