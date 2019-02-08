bd085dd2d6365a3d99b06bed2809be4c1c1baa78
Mike Rossetti <s2t2@users.noreply.github.com>
2019-02-08 14:18:12 -0500

    Make a working request

--- scripts/phonemes_ibm.py
+++ scripts/phonemes_ibm.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
+import json
 import os
-from watson_developer_cloud import TextToSpeechV1, WatsonApiException
+from watson_developer_cloud import SpeechToTextV1, WatsonApiException
 
 load_dotenv()
 
@@ -9,19 +10,21 @@ URL = os.environ.get("SPEECH_TO_TEXT_URL", "https://gateway-wdc.watsonplatform.n
 print("API_KEY:", API_KEY)
 print("URL:", URL)
 
-client = TextToSpeechV1(iam_apikey=API_KEY, url=URL)
+client = SpeechToTextV1(iam_apikey=API_KEY, url=URL)
 print("CLIENT:", type(client))
 
-#voices = client.list_voices().get_result()
-#print(json.dumps(voices, indent=2))
+response = client.list_models().get_result()
+print("MODELS:", type(response)) #> dict
+for m in response["models"]:
+    print("  + ", m["description"])
 
-try:
-    #response = client.get_word(customization_id="1", word="HELLO WORLD") #> Malformed GUID: '1'
-    #response = client.get_word(word="HELLO WORLD") #> get_word() missing 1 required positional argument: 'customization_id'
-    #response = client.get_word(customization_id=None, word="HELLO WORLD") #> ValueError: customization_id must be provided
-    #response = client.get_word(customization_id="GA", word="HELLO WORLD") #> ERROR 400: Malformed GUID: 'GA'
-    response = client.get_word(customization_id="_______", word="HELLO WORLD") #> ERROR 400: Malformed GUID: 'GA'
-    print("RESPONSE")
-    print(type(response))
-except WatsonApiException as ex:
-    print(f"ERROR {str(ex.code)}: {ex.message}")
+#try:
+#    #response = client.get_word(customization_id="1", word="HELLO WORLD") #> Malformed GUID: '1'
+#    #response = client.get_word(word="HELLO WORLD") #> get_word() missing 1 required positional argument: 'customization_id'
+#    #response = client.get_word(customization_id=None, word="HELLO WORLD") #> ValueError: customization_id must be provided
+#    #response = client.get_word(customization_id="GA", word="HELLO WORLD") #> ERROR 400: Malformed GUID: 'GA'
+#    response = client.get_word(customization_id="_______", word="HELLO WORLD") #> ERROR 400: Malformed GUID: 'GA'
+#    print("RESPONSE")
+#    print(type(response))
+#except WatsonApiException as ex:
+#    print(f"ERROR {str(ex.code)}: {ex.message}")
