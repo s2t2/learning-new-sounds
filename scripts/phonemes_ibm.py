ada60bfd128e0ba439030eedc34f81859c2f5a02
Mike Rossetti <s2t2@users.noreply.github.com>
2019-02-08 00:15:00 -0500

    Auth successful but feature requires plan upgrade

--- scripts/phonemes_ibm.py
+++ scripts/phonemes_ibm.py
@@ -1,19 +1,23 @@
+from dotenv import load_dotenv
 import os
 from watson_developer_cloud import TextToSpeechV1, WatsonApiException
 
-IBM_API_KEY = os.environ.get("IBM_API_KEY", "OOPS")
-IBM_URL = os.environ.get("IBM_PASSWORD", "https://gateway-wdc.watsonplatform.net/text-to-speech/api")
-print("CREDENTIALS:", IBM_API_KEY)
+load_dotenv()
 
-client = TextToSpeechV1(iam_apikey=IBM_API_KEY, url=IBM_URL)
+API_KEY = os.environ.get("SPEECH_TO_TEXT_APIKEY", "OOPS")
+URL = os.environ.get("SPEECH_TO_TEXT_URL", "https://gateway-wdc.watsonplatform.net/text-to-speech/api")
+print("API_KEY:", API_KEY)
+print("URL:", URL)
+
+client = TextToSpeechV1(iam_apikey=API_KEY, url=URL)
 print("CLIENT:", type(client))
 
-voices = client.list_voices().get_result()
-print(json.dumps(voices, indent=2))
+#voices = client.list_voices().get_result()
+#print(json.dumps(voices, indent=2))
 
-#try:
-#    response = client.get_word(customization_id="1", word="HELLO WORLD")
-#    print("RESPONSE")
-#    print(type(response))
-#except WatsonApiException as ex:
-#    print(f"ERROR {str(ex.code)}: {ex.message}")
+try:
+    response = client.get_word(customization_id="1", word="HELLO WORLD")
+    print("RESPONSE")
+    print(type(response))
+except WatsonApiException as ex:
+    print(f"ERROR {str(ex.code)}: {ex.message}")
