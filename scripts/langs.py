2e54f052ee83183cf70ecb5e74ad5e86854880db
Mike Rossetti <s2t2@users.noreply.github.com>
2019-02-08 01:07:23 -0500

    Handle file not found errors

--- scripts/langs.py
+++ scripts/langs.py
@@ -22,18 +22,17 @@ for r in recordings:
 
     filepath = os.path.join(os.path.dirname(__file__), "..", "sounds", r["filename"])
 
-    with sr.AudioFile(filepath) as source:
-        audio = client.record(source)
-
     try:
+
+        with sr.AudioFile(filepath) as source:
+            audio = client.record(source)
+
         #transcript = client.recognize_google(audio) #> 'how old is the Brooklyn Bridge'
         #print("TRANSCRIPT: " + transcript)
-
         response = client.recognize_google(audio, language=lang, show_all=True) #> {'alternative': [{'transcript': 'how old is the Brooklyn Bridge', 'confidence': 0.987629}], 'final': True}
         alternative = response["alternative"][0]
         print("TRANSCRIPT: " + alternative["transcript"])
         print("CONFIDENCE: " + str(alternative["confidence"]))
 
     except Exception as e:
-        print("TRANSCRIPT ERROR:", e)
-        next
+        print("ERROR:", e)
