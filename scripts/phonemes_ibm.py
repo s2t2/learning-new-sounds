bbc0775a985203f4f1bd608001d14251606756b2
Mike Rossetti <s2t2@users.noreply.github.com>
2019-02-09 13:57:34 -0500

    Lots of fails

--- scripts/phonemes_ibm.py
+++ scripts/phonemes_ibm.py
@@ -3,8 +3,6 @@ import json
 import os
 from watson_developer_cloud import TextToSpeechV1, WatsonApiException
 
-breakpoint()
-
 load_dotenv()
 
 API_KEY = os.environ.get("SPEECH_TO_TEXT_APIKEY", "OOPS")
@@ -18,22 +16,18 @@ print("CLIENT:", type(client))
 LANG = "en-US" # Allowable values: [de-DE, en-US, en-GB, es-ES, es-LA, es-US, fr-FR, it-IT ,ja-JP, pt-BR]
 
 try:
-    voice_model_response = client.create_voice_model(
-        name="My Custom Model",
-        base_model_name="My Base Model Name",
-        language=LANG,
-        description="to get a valid 'customization_id' value..."
-    ).get_result()
 
+    voice_model_response = client.create_voice_model(name="My Custom Model", language=LANG, description="to get a valid 'customization_id' value...").get_result()
     #> https://github.com/watson-developer-cloud/python-sdk/issues/639
     #> https://stackoverflow.com/questions/54599355/ibm-phoneme-detection-in-python
 
-    breakpoint()
+    #breakpoint()
 
     customization_id = voice_model_response["customization_id"]
+    print(customization_id)
 
-    response = client.get_word(customization_id=customization_id, word="HELLO WORLD")
+    #response = client.get_word(customization_id=customization_id, word="HELLO WORLD")
     print("RESPONSE")
-    print(type(response))
+    #print(type(response))
 except WatsonApiException as ex:
     print(f"ERROR {str(ex.code)}: {ex.message}")
