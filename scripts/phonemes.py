3ad8ee6f42d491beb4fd53b8b3abb8d168702b92
Mike Rossetti <s2t2@users.noreply.github.com>
2019-03-23 17:50:10 -0400

    Refactor

--- scripts/phonemes.py
+++ scripts/phonemes.py
@@ -8,21 +8,18 @@ load_dotenv()
 AUDIO_FILENAME = os.environ.get("AUDIO", "brooklyn.flac")
 AUDIO_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "sounds", AUDIO_FILENAME)
 
-print("AUDIO FILE:" + AUDIO_FILEPATH.replace("scripts/../",""))
+def use_sr():
+    print("AUDIO FILE:", AUDIO_FILEPATH.replace("scripts/../",""))
+    client = sr.Recognizer()
+    print("-----------------------")
+    print("CLIENT:", type(client))
+    with sr.AudioFile(AUDIO_FILEPATH) as source:
+        audio = client.record(source)
+    print("-----------------------")
+    print("AUDIO:", type(audio))
+    response = client.recognize_sphinx(audio) #> 'how old is the Brooklyn Bridge'
+    print("-----------------------")
+    print("TRANSCRIPT:", response.upper())
 
-client = sr.Recognizer()
-
-print("-----------------------")
-print("CLIENT:", type(client))
-
-with sr.AudioFile(AUDIO_FILEPATH) as source:
-    audio = client.record(source)
-
-print("-----------------------")
-print("AUDIO:", type(audio))
-
-response = client.recognize_sphinx(audio) #> 'how old is the Brooklyn Bridge'
-
-print("-----------------------")
-print("RESPONSE:", type(response))
-print("TRANSCRIPT:", response)
+if __name__ == "__main__":
+    use_sr()
